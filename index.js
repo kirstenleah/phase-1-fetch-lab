97c78ea33ac2fac0c480f0356aa548d13c24c511
Kirsten <kirsten.staggs@gmail.com>
2022-04-18 18:29:50 -0600

    commit tree c3346b16cb4bc5e196c0369c027d408030a95a86

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data))
+} 
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +14,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
